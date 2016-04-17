6b575cd46969a37cf6458146e913ad8ca9e94f92
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-04-17 11:13:53 -0700

    condensed code in portfolio.js

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -9,41 +9,11 @@ function Project (opts) {
   this.body = opts.body;
 };
 
-Project.prototype.toAuthors = function () {
-  var $source = $('#author-template').html();
-  var template = Handlebars.compile($source);
-  return template(this);
-};
-
-Project.prototype.toCategories = function () {
-  var $source = $('#category-template').html();
-  var template = Handlebars.compile($source);
-  return template(this);
-};
-
 Project.prototype.toHtml = function(){
-  var $source = $('#projects-template').html();
-  var template = Handlebars.compile($source);
+  var template = Handlebars.compile($('#projects-template').text());
   this.daysAgo = parseInt((new Date() - new Date(this.launchedOn))/60/60/24/1000);
   this.publishStatus = this.launchedOn ? 'launched ' + this.daysAgo + ' days ago' : '(draft)';
   return template(this);
-  // var $newProject = $('article.template').clone();
-  // $newProject.removeClass('template');
-  //
-  // $newProject.find('.project-body').html(this.body);
-  // $newProject.attr('data-category', this.category);
-  // $newProject.attr('data-author', this.author);
-  // $newProject.find('address').html(this.author);
-  // $newProject.find('h1').html(this.title);
-  // $newProject.find('a').attr('href', this.projectLink);
-  // $newProject.find('time[pubdate]').attr('datetime', this.launchedOn);
-  // $newProject.find('time[pubdate]').attr('title', this.launchedOn);
-  //
-  // $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.launchedOn))/60/60/24/1000) + ' days ago');
-  //
-  // $newProject.append('<hr>');
-  //
-  // return $newProject;
 };
 
 rawData.sort(function(a,b) {
@@ -56,12 +26,4 @@ rawData.forEach(function(ele) {
 
 projects.forEach(function(a){
   $('#projects').append(a.toHtml());
-});
-
-projects.forEach(function(a){
-  $('#author-filter').append(a.toAuthors());
-});
-
-projects.forEach(function(a){
-  $('#category-filter').append(a.toCategories());
 });
