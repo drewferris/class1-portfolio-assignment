1933a1d60ab6d9c5c6708254d04c6a9458ace9f3
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-04-19 16:46:03 -0700

    made a comments section for all my old commented out code in case i want to use them later, started making a stats section in the footer

---
+++ scripts/commentedout.js
@@ -0,0 +1,71 @@
+//FROM using this array so that categories filter didnt triple
+
+// var categories = [];
+
+//FROM portfolio.js lines 17-29 BEFORE project constructor
+//USE was used when I was trying to use handlebars for putting my filters on to the html instead of using populateFilters in the portfolioView1.js
+
+// var etag = '';
+
+// Project.prototype.toAuthors = function () {
+//   var $source = $('#author-template').html();
+//   var template = Handlebars.compile($source);
+//   return template(this);
+// };
+
+// Project.prototype.toCategories = function () {
+//   var $source = $('#category-template').html();
+//   var template = Handlebars.compile($source);
+//   return template(this);
+// };
+
+// $('#author-filter').append(a.toHtml('author'));
+// if (categories.indexOf(a.category) === -1) {
+//   $('#category-filter').append(a.toHtml('category'));
+//   categories.push(a.category);
+// }
+// });
+
+// projects.forEach(function(a){
+//   $('#author-filter').append(a.toAuthors());
+// });
+//
+// projects.forEach(function(a){
+//   $('#category-filter').append(a.toCategories());
+// });
+
+//  FROM logic inside fetchALl
+
+//         if (localStorage.etag === etag) {
+//           Project.loadAll(JSON.parse(localStorage.projects));
+//           projectView.initIndexPage();
+//         } else {
+//           Project.jsonfetch();
+//         }
+//       }
+//     });
+//   } else {
+//     Project.jsonfetch();
+//   }
+// };
+
+//  FROM old get request that was replaced by getAll
+// Project.jsonfetch = function() {
+//   $.ajax ({
+//     type: 'GET',
+//     dataType: 'json',
+//     url: 'data/projects.json',
+//     success: function(data, message, xhr) {
+//
+//
+//       Project.loadAll(data);
+//       localStorage.setItem('projects', JSON.stringify(data));
+//       projectView.initIndexPage();
+//       etag = (xhr.getResponseHeader('ETag'));
+//       localStorage.setItem('etag', etag);
+//     },
+//     error: function(data, message, xhr){
+//       console.log('url');
+//     }
+//   });
+// };
--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,7 +1,5 @@
 (function(module) {
 
-  var categories = [];
-
   function Project (opts) {
     this.title = opts.title;
     this.author = opts.author;
@@ -11,23 +9,7 @@
     this.body = opts.body;
   };
 
-
   Project.all = [];
-
-  // var etag = '';
-
-  // Project.prototype.toAuthors = function () {
-  //   var $source = $('#author-template').html();
-  //   var template = Handlebars.compile($source);
-  //   return template(this);
-  // };
-
-  // Project.prototype.toCategories = function () {
-  //   var $source = $('#category-template').html();
-  //   var template = Handlebars.compile($source);
-  //   return template(this);
-  // };
-
 
   Project.prototype.toHtml = function(){
     // var $source = $('#' + templateId + '-template').html();
@@ -45,29 +27,11 @@
       return (new Date(b.launchedOn)) - (new Date(a.launchedOn));
     });
 
-    // dataPassedIn.forEach(function(ele) {
     Project.all = dataPassedIn.map(function(ele) {
       return new Project(ele);
     });
-
-    // projects.forEach(function(a){
-    //   $('#projects').append(a.toHtml());
-    // });
   };
-    // $('#author-filter').append(a.toHtml('author'));
-    // if (categories.indexOf(a.category) === -1) {
-    //   $('#category-filter').append(a.toHtml('category'));
-    //   categories.push(a.category);
-    // }
-  // });
 
-  // projects.forEach(function(a){
-  //   $('#author-filter').append(a.toAuthors());
-  // });
-  //
-  // projects.forEach(function(a){
-  //   $('#category-filter').append(a.toCategories());
-  // });
   Project.fetchAll = function(callback) {
     if (localStorage.projects) {
       $.ajax({
@@ -88,18 +52,6 @@
       Project.getAll();
     }
   };
-  //         if (localStorage.etag === etag) {
-  //           Project.loadAll(JSON.parse(localStorage.projects));
-  //           projectView.initIndexPage();
-  //         } else {
-  //           Project.jsonfetch();
-  //         }
-  //       }
-  //     });
-  //   } else {
-  //     Project.jsonfetch();
-  //   }
-  // };
 
   Project.getAll = function() {
     $.getJSON('/data/projects.json', function(responseData) {
@@ -109,24 +61,7 @@
     });
   };
 
-  // Project.jsonfetch = function() {
-  //   $.ajax ({
-  //     type: 'GET',
-  //     dataType: 'json',
-  //     url: 'data/projects.json',
-  //     success: function(data, message, xhr) {
-  //
-  //
-  //       Project.loadAll(data);
-  //       localStorage.setItem('projects', JSON.stringify(data));
-  //       projectView.initIndexPage();
-  //       etag = (xhr.getResponseHeader('ETag'));
-  //       localStorage.setItem('etag', etag);
-  //     },
-  //     error: function(data, message, xhr){
-  //       console.log('url');
-  //     }
-  //   });
-  // };
+  
+
   module.Project = Project;
 })(window);
