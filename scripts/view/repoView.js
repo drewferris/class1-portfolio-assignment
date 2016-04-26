4a465c19a6d3e1274d9482abd587a94531486fe1
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-04-26 15:58:17 -0700

    made repoVIew page

---
+++ scripts/view/repoView.js
@@ -0,0 +1,19 @@
+(function(module) {
+  var repoView = {};
+
+  var ui = function() {
+    var $about = $('#about');
+
+    $about.find('section section ul').empty();
+    $about.show().siblings().hide();
+  };
+
+  var render = Handlebars.compile($('#repo-template').text());
+
+  repoView.index = function() {
+    ui();
+    $('#about section ul').append(repos.with('name').map(render));
+  };
+
+  module.repoView = repoView;
+})(window);
