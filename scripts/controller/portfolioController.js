9fba62b023e058d844f3deefdebe4c70a7b9bdc3
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-04-28 23:35:27 -0700

    added some things

--- scripts/controller/portfolioController.js
+++ scripts/controller/portfolioController.js
@@ -1,11 +1,53 @@
 (function(module) {
   var portfolioController = {};
 
-  portfolioController.index = function() {
-    Project.fetchAll(projectView.initIndexPage);
+  Project.createTable();
+  debugger;
+  portfolioController.index = function(ctx, next) {
 
-    $('main > section').hide();
-    $('#projects').show();
+    projectView.index(ctx.projects);
+  };
+
+  portfolioController.loadById = function(ctx, next) {
+    var portfolioData =     function(project) {
+      ctx.projects = project;
+      next();
+    };
+
+    Project.findWhere('id', ctx.params.id, projectData);
+  };
+
+  portfolioController.loadByAuthor = function(ctx, next) {
+    var authorData = function(projectsByAuthor) {
+      ctx.projects = projectsByAuthor;
+      next();
+    };
+
+    Project.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
+  };
+
+  portfolioController.loadByCategory = function(ctx, next) {
+    var categoryData = function(projectsInCategory) {
+      ctx.projects = projectsInCategory;
+      next();
+    };
+
+    Project.findWhere('author', ctx.params.categoryName.replace('+', ' '), categoryData);
+  };
+
+
+  portfolioController.loadAll = function(ctx, next) {
+    var portfolioData = function(allProjects) {
+      ctx.projects = Project.all;
+      next();
+    };
+
+    if (Project.all.length) {
+      ctx.projects = Project.all;
+      next();
+    } else {
+      Project.fetchAll(portfolioData);
+    }
   };
 
   module.portfolioController = portfolioController;
